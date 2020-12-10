cb0e9d58fdbdacd054d11502b60d8b05df27a223
srupnar1 <srupnar1@binghamton.edu>
2020-12-10 17:01:29 -0500

    initial check-in for Restaurants-review-and-rating-system project

---
+++ Restaurantsreview/src/test/java/com/restaurantsreview/RestaurantsreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantsreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantsreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
